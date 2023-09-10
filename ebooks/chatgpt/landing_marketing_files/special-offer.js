4b1ce501831b8d02d1bbc24be70bc157a19a8197
Szymon Paluch <sp@riot.agency>
2023-09-10 22:34:55 +0200

    Add Special Offer section and functionality: - Added a new section in `index.html` for a time-limited special offer - Imported new image asset `bonus.png` for the special offer - Created script `special-offer.js` to implement a countdown timer for the offer - Script also removes the special offer after 60 minutes - Integrated the new script in `index.html` - Updated query parameter functions in `index.html` and `midjourney/index.html` to clean up empty keys - Early return added if no query parameters are present

---
+++ ebooks/chatgpt/landing_marketing_files/special-offer.js
@@ -0,0 +1,51 @@
+// JavaScript code to handle the special offer
+document.addEventListener('DOMContentLoaded', function() {
+    // 1. Save the page load time in localStorage if not saved previously
+    if (!localStorage.getItem('pageLoadTime')) {
+      localStorage.setItem('pageLoadTime', new Date().getTime());
+    }
+  
+    const pageLoadTime = Number(localStorage.getItem('pageLoadTime'));
+    const currentTime = new Date().getTime();
+    const timeDifferenceMinutes = (currentTime - pageLoadTime) / 1000 / 60;
+  
+    // 2. Remove the specialOffer section if more than 60 minutes have passed
+    if (timeDifferenceMinutes >= 60) {
+      const specialOfferSection = document.getElementById('specialOffer');
+      if (specialOfferSection) {
+        specialOfferSection.remove();
+      }
+      return;
+    }
+  
+    // 3. Update the countdown timer in the specialOffer section
+    const specialOfferCounterMinutes = document.getElementById('specialOfferCounterMinutes');
+    const specialOfferCounterSeconds = document.getElementById('specialOfferCounterSeconds');
+  
+    function updateCounter() {
+      const currentTime = new Date().getTime();
+      const timeLeftSeconds = 60 * 60 - (currentTime - pageLoadTime) / 1000;
+      
+      if (timeLeftSeconds <= 0) {
+        // 4. Remove the specialOffer section after 60 minutes
+        const specialOfferSection = document.getElementById('specialOffer');
+        if (specialOfferSection) {
+          specialOfferSection.remove();
+        }
+        clearInterval(interval);
+        return;
+      }
+      
+      const minutes = Math.floor(timeLeftSeconds / 60);
+      const seconds = Math.floor(timeLeftSeconds % 60);
+      
+      if (specialOfferCounterMinutes && specialOfferCounterSeconds) {
+        specialOfferCounterMinutes.textContent = minutes;
+        specialOfferCounterSeconds.textContent = seconds;
+      }
+    }
+  
+    const interval = setInterval(updateCounter, 1000);
+    updateCounter();
+  });
+  
